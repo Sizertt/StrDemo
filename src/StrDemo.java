8bfd75522936a050d56833e2db86e71751cf2f6d
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-07 07:39:26 +0300

    StrDemo

---
+++ src/StrDemo.java
@@ -0,0 +1,15 @@
+public class StrDemo {
+    public  static void main(String[] args) {
+
+    // Для перевода каретки(Начала новой строки)
+    // Служит код \n
+    System.out.println("Первая строка\nВторая строка") ;
+
+    // Знак табуляции помогает оформить выводимый текст
+    // В колонки.Он выводится с помощью кода \t
+    System.out.println("A\tB\tC");
+    System.out.println("D\tE\tF");
+
+    } // main(String[]) method
+    } // StrDemo class
+
